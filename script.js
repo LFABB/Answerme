a959a04a6622a8e674a7c6bc967ea4550f1c3e9d
MasterCoder <lucas21mayo@gmail.com>
2025-02-06 10:00:09 -0600

    Full and rebuild version of the project

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
+document.getElementById("btn2").addEventListener("click", ()=>{
+    let x = Math.random()*500;
+    let y = Math.random()*500;
+    document.getElementById("btn2").style.top = `${y}px`;
+    document.getElementById("btn2").style.right = `${x}px`;
+    
+});
 
-btn1.addEventListener("click", function(){
-    alert("Im blushing right now...")
-})
+let alt = false;
+function toggle(){
+    if (alt == false){
+        document.getElementById("lang").innerHTML = "ES";
+        document.getElementById("btn1").innerHTML = "Yes";
+        document.getElementById("message").innerHTML = "So then are you hiring me?";
+        alt = true;
+    }else{
+        document.getElementById("lang").innerHTML = "EN";
+        document.getElementById("btn1").innerHTML = "Si";
+        document.getElementById("message").innerHTML = "Entonces, me contratas?";
+        alt = false;
+    }
+};
 
-btn2.addEventListener("click", function(){
-    let x = Math.random()*400;
-    let y = Math.random()*400;
-    btn2.style.setProperty("top", `${y}px`);
-    btn2.style.setProperty("right", `${x}px`);
-})
+document.querySelectorAll("button").forEach(x=>{x.style.width = "40px"});
+document.querySelectorAll("button").forEach(x=>{x.style.height = "25px"});
+document.querySelectorAll("button").forEach(x=>{x.style.border = "2px solid black"});
+document.querySelectorAll("button").forEach(x=>{x.style.borderRadius = "5px"});
+document.querySelectorAll("button").forEach(x=>{x.style.background = "none"});
+document.querySelectorAll("button").forEach(x=>{x.style.cursor = "pointer"});
